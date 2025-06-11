73788c4e4fe9a7417b23bb27040c3d7fd0d5bf01
ayusr0919 <ayusr0919@gmail.com>
2025-06-11 10:43:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 from typing import Final
-import sklearn
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
